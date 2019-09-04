cilium/proxy

commit b5f584caa50303115a3f7c059d5fdcf85e8eb8d5

istio: Update to 1.2.0
Update Istio dependency to 1.2.0, and Envoy dependency accordingly.

Update Cilium filters to compile with the updated Envoy dependency.

Update generated Envoy APIs, which should be backwards compatible.

Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

cilium/accesslog.cc
@@ -56,6 +56,13 @@ AccessLog::AccessLog(std::string path) : path_(path), fd_(-1), open_count_(1) {}
 
 AccessLog::~AccessLog() {}
 
+#define CONST_STRING_VIEW(NAME, STR) const absl::string_view NAME = {STR, sizeof (STR) - 1}
+
+CONST_STRING_VIEW(pathSV, ":path");
+CONST_STRING_VIEW(methodSV, ":method");
+CONST_STRING_VIEW(authoritySV, ":authority");
+CONST_STRING_VIEW(xForwardedProtoSV, "x-forwarded-proto");
+
 void AccessLog::Entry::InitFromRequest(
     std::string policy_name, bool ingress, const Network::Connection *conn,
     const Http::HeaderMap &headers, const StreamInfo::StreamInfo &info) {
@@ -85,11 +92,12 @@ void AccessLog::Entry::InitFromRequest(
   if (conn) {
     const auto& options_ = conn->socketOptions();
     if (options_) {
-      const Cilium::SocketMarkOption* option = nullptr;
+      const Cilium::SocketOption* option = nullptr;
       for (const auto& option_: *options_) {
-	option = dynamic_cast<const Cilium::SocketMarkOption*>(option_.get());
+	option = dynamic_cast<const Cilium::SocketOption*>(option_.get());
 	if (option) {
 	  entry.set_source_security_id(option->identity_);
+	  entry.set_destination_security_id(option->destination_identity_);
 	  break;
 	}
       }
@@ -97,39 +105,45 @@ void AccessLog::Entry::InitFromRequest(
 	ENVOY_CONN_LOG(warn, "accesslog: Cilium Socket Option not found", *conn);
       }
     }
-    entry.set_source_address(conn->remoteAddress()->asString());
-    entry.set_destination_address(conn->localAddress()->asString());
+    auto source_address = conn->remoteAddress();
+    if (source_address != nullptr) {
+      entry.set_source_address(source_address->asString());
+    }
+    auto destination_address = conn->localAddress();
+    if (destination_address != nullptr) {
+      entry.set_destination_address(destination_address->asString());
+    }
   }
 
   // request headers
   headers.iterate(
       [](const Http::HeaderEntry &header, void *entry_) -> Http::HeaderMap::Iterate {
-        const Http::HeaderString &key = header.key();
-        const char* value = header.value().c_str();
+        const absl::string_view key = header.key().getStringView();
+        const absl::string_view value = header.value().getStringView();
         auto entry = static_cast<::cilium::HttpLogEntry *>(entry_);
 
-        if (key == ":path") {
-          entry->set_path(value);
-        } else if (key == ":method") {
-          entry->set_method(value);
-        } else if (key == ":authority") {
-          entry->set_host(value);
-        } else if (key == "x-forwarded-proto") {
+        if (key == pathSV) {
+          entry->set_path(value.data(), value.size());
+        } else if (key == methodSV) {
+          entry->set_method(value.data(), value.size());
+        } else if (key == authoritySV) {
+          entry->set_host(value.data(), value.size());
+        } else if (key == xForwardedProtoSV) {
           // Envoy sets the ":scheme" header later in the router filter
           // according to the upstream protocol (TLS vs. clear), but we want to
           // get the downstream scheme, which is provided in
           // "x-forwarded-proto".
-          entry->set_scheme(value);
+          entry->set_scheme(value.data(), value.size());
         } else {
           ::cilium::KeyValue *kv = entry->add_headers();
-          kv->set_key(key.c_str());
-          kv->set_value(value);
+          kv->set_key(key.data(), key.size());
+          kv->set_value(value.data(), value.size());
         }
         return Http::HeaderMap::Iterate::Continue;
       },
       http_entry);
 
-    entry.set_is_ingress(ingress);
+  entry.set_is_ingress(ingress);
 }
 
 void AccessLog::Entry::UpdateFromResponse(
@@ -150,7 +164,7 @@ void AccessLog::Entry::UpdateFromResponse(
     const Http::HeaderEntry *status_entry = headers.Status();
     if (status_entry) {
       uint64_t status;
-      if (StringUtil::atoull(status_entry->value().c_str(), status, 10)) {
+      if (StringUtil::atoull(status_entry->value().getStringView().data(), status, 10)) {
         http_entry->set_status(status);
       }
     }

cilium_integration_test.cc
@@ -487,7 +487,9 @@ public:
     auto response = codec_client_->makeHeaderOnlyRequest(headers);
     response->waitForEndStream();
 
-    EXPECT_STREQ("403", response->headers().Status()->value().c_str());
+    uint64_t status;
+    StringUtil::atoull(response->headers().Status()->value().getStringView().data(), status, 10);
+    EXPECT_EQ(403, status);
   }
 
   void Accepted(Http::TestHeaderMapImpl&& headers) {
@@ -496,7 +498,9 @@ public:
     codec_client_ = makeHttpConnection(lookupPort("http"));
     auto response = sendRequestAndWaitForResponse(headers, 0, default_response_headers_, 0);
 
-    EXPECT_STREQ("200", response->headers().Status()->value().c_str());
+    uint64_t status;
+    StringUtil::atoull(response->headers().Status()->value().getStringView().data(), status, 10);
+    EXPECT_EQ(200, status);
   }
 
   void InvalidHostMap(const std::string& config, const char* exmsg) {
@@ -814,7 +818,9 @@ TEST_P(CiliumIntegrationTest, DuplicatePort) {
   auto response = codec_client_->makeHeaderOnlyRequest(headers);
   response->waitForEndStream();
 
-  EXPECT_STREQ("403", response->headers().Status()->value().c_str());
+  uint64_t status;
+  StringUtil::atoull(response->headers().Status()->value().getStringView().data(), status, 10);
+  EXPECT_EQ(403, status);
 }
 
 class CiliumIntegrationEgressTest : public CiliumHttpIntegrationTest {
@@ -1574,5 +1580,69 @@ TEST_P(CiliumGoBlocktesterIntegrationTest, CiliumGoBlockParserInjectBufferOverfl
   ASSERT_TRUE(fake_upstream_connection->waitForHalfClose());
   ASSERT_TRUE(fake_upstream_connection->waitForDisconnect());
 }
+
+namespace Cilium {
+
+extern const absl::string_view pathSV;
+extern const absl::string_view methodSV;
+extern const absl::string_view authoritySV;
+extern const absl::string_view xForwardedProtoSV;
+
+class CiliumTest : public testing::Test {
+protected:
+  Event::SimulatedTimeSystem time_system_;
+  Network::Address::InstanceConstSharedPtr local_address_;
+  Network::Address::InstanceConstSharedPtr remote_address_;  
+};
+
+TEST_F(CiliumTest, AccessLog) {
+  Http::TestHeaderMapImpl headers{
+    {":method", "GET"},
+      {":path", "/"},
+	{":authority", "host"},
+	  {"x-forwarded-proto", "http"},
+	    {"x-request-id", "ba41267c-cfc2-4a92-ad3e-cd084ab099b4"}};
+  NiceMock<StreamInfo::MockStreamInfo> stream_info;
+  stream_info.protocol_ = Http::Protocol::Http11;
+  stream_info.start_time_ = time_system_.systemTime();
+  Network::MockConnection connection;
+  Network::Socket::OptionsSharedPtr options = std::make_shared<Network::Socket::Options>();
+  options->push_back(std::make_shared<Cilium::SocketOption>(nullptr, nullptr, 1, 173, true, 80, 10000, "1.2.3.4"));
+  local_address_ = std::make_shared<Network::Address::Ipv4Instance>("1.2.3.4", 80);
+  remote_address_ = std::make_shared<Network::Address::Ipv4Instance>("5.6.7.8", 45678);
+
+  ENVOY_LOG_MISC(error, "source_address: {}", remote_address_->asString());
+  ENVOY_LOG_MISC(error, "destination_address: {}", local_address_->asString());
+
+  EXPECT_CALL(connection, socketOptions()).WillOnce(testing::ReturnRef(options));
+  EXPECT_CALL(connection, localAddress()).WillRepeatedly(testing::ReturnRef(local_address_));
+  EXPECT_CALL(connection, remoteAddress()).WillRepeatedly(testing::ReturnRef(remote_address_));
+
+  AccessLog::Entry log;
+
+  log.InitFromRequest("1.2.3.4", true, &connection, headers, stream_info);
+
+  EXPECT_EQ(log.entry.is_ingress(), true);
+  EXPECT_EQ(log.entry.entry_type(), ::cilium::EntryType::Request);
+  EXPECT_NE(log.entry.timestamp(), 0);
+  EXPECT_STREQ(log.entry.policy_name().c_str(), "1.2.3.4");
+  EXPECT_STREQ("1.2.3.4:80", log.entry.destination_address().c_str());
+  EXPECT_STREQ("5.6.7.8:45678", log.entry.source_address().c_str());
+  EXPECT_EQ(1, log.entry.source_security_id());
+  EXPECT_EQ(173, log.entry.destination_security_id());
+
+  EXPECT_EQ(log.entry.has_http(), true);
+  EXPECT_EQ(::cilium::HttpProtocol::HTTP11, log.entry.http().http_protocol());
+  EXPECT_STREQ("/", log.entry.http().path().c_str());
+  EXPECT_STREQ("GET", log.entry.http().method().c_str());
+  EXPECT_STREQ("host", log.entry.http().host().c_str());
+  EXPECT_STREQ("http", log.entry.http().scheme().c_str());
+
+  EXPECT_EQ(log.entry.http().headers_size(), 1);
+  EXPECT_STREQ(log.entry.http().headers(0).key().c_str(), "x-request-id");
+  EXPECT_STREQ(log.entry.http().headers(0).value().c_str(), "ba41267c-cfc2-4a92-ad3e-cd084ab099b4");
+}
+
+} // namespace Cilium
 
 } // namespace Envoy

BAZEL_VERSION
@@ -1,1 +1,1 @@
-0.24.1
+0.25.0

Makefile
@@ -34,7 +34,7 @@ CLANG_FORMAT ?= clang-format
 BUILDIFIER ?= buildifier
 STRIP ?= $(QUIET) strip
 
-ISTIO_VERSION = 1.1.7
+ISTIO_VERSION = 1.2.0
 
 DOCKER=$(QUIET)docker
 

WORKSPACE
@@ -7,8 +7,8 @@ workspace(name = "cilium")
 #
 # No other line in this file may have ENVOY_SHA followed by an equals sign!
 #
-ENVOY_SHA = "ac7aa5ac8a815e5277b4d4659c5c02145fa1d56f"
-ENVOY_SHA256 = "3f13facc893ef0c5063c7391a1ffca8de0f52425c8a7a49ef45e69dbb5e7304b"
+ENVOY_SHA = "4f5b5e101a081e05924990b1903d9d46553558d4"
+ENVOY_SHA256 = "59b1599b8847543d7614c5507a33f14f9de49c34ac112cf0d6e082392294eaff"
 
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
@@ -46,9 +46,9 @@ go_register_toolchains(go_version = GO_VERSION)
 
 # Dependencies for Istio filters.
 # Cf. https://github.com/istio/proxy.
-# Version 1.1.7
-ISTIO_PROXY_SHA = "73fa9b1f29f91029cc2485a685994a0d1dbcde21"
-ISTIO_PROXY_SHA256 = "4c40c6970e263a947bae750bac9228e1560a55c8457db7257f7a34e7c58a61a8"
+# Version 1.2.0
+ISTIO_PROXY_SHA = "7767d3a6fd8def76b44f5c03283ba3f2f9dd74a9"
+ISTIO_PROXY_SHA256 = "24ad5225b7387a710987311105ed7c295df9aaddae569492c758dca318dea99f"
 
 http_archive(
     name = "istio_proxy",

cilium/l7policy.cc
@@ -144,7 +144,7 @@ Http::FilterHeadersStatus AccessFilter::decodeHeaders(Http::HeaderMap& headers, 
     config_->stats_.access_denied_.inc();
 
     // Return a 403 response
-    callbacks_->sendLocalReply(Http::Code::Forbidden, config_->denied_403_body_, nullptr, absl::nullopt);
+    callbacks_->sendLocalReply(Http::Code::Forbidden, config_->denied_403_body_, nullptr, absl::nullopt, absl::string_view());
     return Http::FilterHeadersStatus::StopIteration;
   }
 

go/envoy/admin/v2alpha/clusters.pb.go
@@ -231,6 +231,11 @@ type HostHealthStatus struct {
 	FailedOutlierCheck bool `protobuf:"varint,2,opt,name=failed_outlier_check,json=failedOutlierCheck,proto3" json:"failed_outlier_check,omitempty"`
 	// The host is currently being marked as degraded through active health checking.
 	FailedActiveDegradedCheck bool `protobuf:"varint,4,opt,name=failed_active_degraded_check,json=failedActiveDegradedCheck,proto3" json:"failed_active_degraded_check,omitempty"`
+	// The host has been removed from service discovery, but is being stabilized due to active
+	// health checking.
+	PendingDynamicRemoval bool `protobuf:"varint,5,opt,name=pending_dynamic_removal,json=pendingDynamicRemoval,proto3" json:"pending_dynamic_removal,omitempty"`
+	// The host has not yet been health checked.
+	PendingActiveHc bool `protobuf:"varint,6,opt,name=pending_active_hc,json=pendingActiveHc,proto3" json:"pending_active_hc,omitempty"`
 	// Health status as reported by EDS. Note: only HEALTHY and UNHEALTHY are currently supported
 	// here.
 	// TODO(mrice32): pipe through remaining EDS health status possibilities.
@@ -286,6 +291,20 @@ func (m *HostHealthStatus) GetFailedActiveDegradedCheck() bool {
 	return false
 }
 
+func (m *HostHealthStatus) GetPendingDynamicRemoval() bool {
+	if m != nil {
+		return m.PendingDynamicRemoval
+	}
+	return false
+}
+
+func (m *HostHealthStatus) GetPendingActiveHc() bool {
+	if m != nil {
+		return m.PendingActiveHc
+	}
+	return false
+}
+
 func (m *HostHealthStatus) GetEdsHealthStatus() core.HealthStatus {
 	if m != nil {
 		return m.EdsHealthStatus
@@ -303,40 +322,43 @@ func init() {
 func init() { proto.RegisterFile("envoy/admin/v2alpha/clusters.proto", fileDescriptor_c6251a3a957f478b) }
 
 var fileDescriptor_c6251a3a957f478b = []byte{
-	// 550 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x93, 0xd1, 0x6e, 0xd3, 0x3c,
-	0x14, 0xc7, 0x95, 0xae, 0xdb, 0xd7, 0xef, 0x74, 0x65, 0xc3, 0x43, 0x10, 0xaa, 0x49, 0x6d, 0x23,
-	0x90, 0x7a, 0x95, 0xa0, 0x30, 0xc1, 0x05, 0x17, 0xa8, 0xdb, 0x90, 0x26, 0xd0, 0x44, 0x95, 0x21,
-	0x24, 0xb8, 0x89, 0x4c, 0x7c, 0x58, 0x0c, 0x69, 0x1d, 0xd9, 0x6e, 0xa0, 0x4f, 0xc1, 0x25, 0xcf,
-	0xc8, 0x5b, 0xa0, 0xd8, 0xee, 0x9a, 0xb2, 0xee, 0xce, 0xf6, 0xf9, 0xfd, 0xed, 0xf3, 0x3f, 0xe7,
-	0x18, 0x02, 0x9c, 0x57, 0x62, 0x19, 0x51, 0x36, 0xe3, 0xf3, 0xa8, 0x8a, 0x69, 0x51, 0xe6, 0x34,
-	0xca, 0x8a, 0x85, 0xd2, 0x28, 0x55, 0x58, 0x4a, 0xa1, 0x05, 0x39, 0x32, 0x4c, 0x68, 0x98, 0xd0,
-	0x31, 0xfd, 0xd1, 0x36, 0xe1, 0x0c, 0xb5, 0xe4, 0x99, 0xd3, 0xf5, 0x07, 0x0e, 0x29, 0x79, 0x54,
-	0xc5, 0x51, 0x26, 0x24, 0x46, 0x94, 0x31, 0x89, 0x6a, 0x05, 0x3c, 0xb9, 0x0d, 0xe4, 0x48, 0x0b,
-	0x9d, 0xa7, 0x59, 0x8e, 0xd9, 0x77, 0x47, 0xf9, 0x96, 0xd2, 0xcb, 0x12, 0xa3, 0x12, 0x65, 0x86,
-	0x73, 0x6d, 0x23, 0xc1, 0x27, 0xe8, 0x9c, 0xb9, 0x54, 0xc9, 0x25, 0x1c, 0xba, 0xb4, 0x53, 0xa5,
-	0xa9, 0x5e, 0x28, 0x54, 0xbe, 0x37, 0xdc, 0x19, 0x77, 0xe3, 0x20, 0xdc, 0x92, 0x7f, 0xe8, 0x84,
-	0x57, 0x86, 0x4d, 0x0e, 0xb2, 0xe6, 0x16, 0x55, 0xf0, 0xc7, 0x83, 0xde, 0x06, 0x42, 0x08, 0xb4,
-	0xe7, 0x74, 0x86, 0xbe, 0x37, 0xf4, 0xc6, 0xff, 0x27, 0x66, 0x4d, 0x02, 0xe8, 0x51, 0xc6, 0x90,
-	0xa5, 0x15, 0xa7, 0x29, 0x2d, 0xb9, 0xdf, 0x1a, 0x7a, 0xe3, 0x4e, 0xd2, 0x35, 0x87, 0x1f, 0x39,
-	0x9d, 0x94, 0x9c, 0x7c, 0x86, 0x81, 0x5a, 0x64, 0x19, 0x2a, 0x95, 0x4a, 0xaa, 0x31, 0xc5, 0x6f,
-	0x98, 0x69, 0x2e, 0xe6, 0xa9, 0xce, 0x25, 0xaa, 0x5c, 0x14, 0xcc, 0xdf, 0x19, 0x7a, 0xe3, 0x6e,
-	0x7c, 0xe4, 0xf2, 0xac, 0x8d, 0x86, 0x53, 0x6b, 0x34, 0x39, 0x76, 0xda, 0x84, 0x6a, 0x7c, 0xe3,
-	0x94, 0x1f, 0x56, 0x42, 0x72, 0x0e, 0xbd, 0x5c, 0x28, 0xbd, 0x76, 0xdc, 0x36, 0x8e, 0x07, 0x5b,
-	0x1d, 0x5f, 0x08, 0xa5, 0x9d, 0xdd, 0xfd, 0xfc, 0x66, 0x8d, 0x2a, 0xf8, 0xdd, 0x02, 0x58, 0x07,
-	0xc9, 0x09, 0xfc, 0xe7, 0xda, 0x64, 0xbc, 0x76, 0xe3, 0xfe, 0xea, 0xba, 0x92, 0x87, 0x55, 0x1c,
-	0xd6, 0x7d, 0x0a, 0x27, 0x96, 0x48, 0x56, 0x28, 0x79, 0x09, 0xbb, 0x75, 0x16, 0xca, 0x6f, 0x99,
-	0x14, 0x46, 0x5b, 0x53, 0xb8, 0xe2, 0xb3, 0xb2, 0xc0, 0x4b, 0x33, 0x25, 0x89, 0xe5, 0xc9, 0x5b,
-	0xe8, 0xb9, 0xa6, 0x5b, 0x17, 0xae, 0x1a, 0x4f, 0xef, 0xf4, 0x70, 0x61, 0xe8, 0x1b, 0x27, 0x8d,
-	0x1d, 0x79, 0x01, 0xfb, 0xcd, 0x5a, 0xfb, 0xed, 0xbb, 0x0b, 0xdb, 0x6d, 0x14, 0x96, 0x3c, 0x84,
-	0xbd, 0x1f, 0xc8, 0xaf, 0x73, 0xed, 0xef, 0x0e, 0xbd, 0x71, 0x2f, 0x71, 0xbb, 0xe0, 0x57, 0x0b,
-	0x0e, 0xff, 0x7d, 0x92, 0xbc, 0x82, 0xfe, 0x57, 0xca, 0x0b, 0x64, 0x29, 0xcd, 0x34, 0xaf, 0x30,
-	0x6d, 0xce, 0xac, 0x29, 0x59, 0x27, 0x79, 0x64, 0x89, 0x89, 0x01, 0xac, 0xfa, 0xac, 0x0e, 0x93,
-	0x67, 0xf0, 0xc0, 0x89, 0xc5, 0x42, 0x17, 0x1c, 0xa5, 0x93, 0xd9, 0xc1, 0x21, 0x36, 0xf6, 0xde,
-	0x86, 0xac, 0xe2, 0x35, 0x1c, 0x6f, 0x3e, 0xc7, 0xf0, 0x5a, 0xd2, 0x7a, 0xe8, 0xac, 0xb2, 0x6d,
-	0x94, 0x8f, 0x9b, 0x0f, 0x9e, 0x3b, 0xc2, 0x5e, 0xf0, 0x0e, 0xee, 0x23, 0x53, 0xe9, 0xed, 0x22,
-	0xdf, 0x5b, 0x0f, 0x4a, 0xa3, 0xb3, 0x1b, 0xe5, 0x3d, 0x40, 0xa6, 0x9a, 0x07, 0xa7, 0x27, 0x30,
-	0xe2, 0xc2, 0xaa, 0x4a, 0x29, 0x7e, 0x2e, 0xb7, 0x75, 0xe9, 0x74, 0xf5, 0x73, 0xd4, 0xb4, 0xfe,
-	0xa6, 0x53, 0xef, 0xcb, 0x9e, 0xf9, 0xaf, 0xcf, 0xff, 0x06, 0x00, 0x00, 0xff, 0xff, 0x5d, 0xc8,
-	0x6f, 0xca, 0x6e, 0x04, 0x00, 0x00,
+	// 597 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x94, 0xdf, 0x6e, 0xd3, 0x30,
+	0x14, 0xc6, 0x95, 0xae, 0x1b, 0xc5, 0x5d, 0xd9, 0xe6, 0x01, 0x0b, 0xd5, 0xa4, 0x75, 0x11, 0x48,
+	0x15, 0x17, 0x09, 0x2a, 0xd3, 0xb8, 0xe0, 0x02, 0xed, 0x0f, 0xd2, 0x04, 0x9a, 0x98, 0x32, 0x84,
+	0x04, 0x37, 0x96, 0xb1, 0x0f, 0x8b, 0x21, 0x8d, 0x23, 0xdb, 0x0d, 0xf4, 0x49, 0x78, 0x46, 0xae,
+	0x79, 0x01, 0x14, 0xdb, 0xd9, 0x52, 0xd6, 0xdd, 0xd5, 0xf9, 0x7e, 0x5f, 0x7c, 0xbe, 0x73, 0x4e,
+	0x83, 0x22, 0x28, 0x2a, 0x39, 0x4f, 0x28, 0x9f, 0x8a, 0x22, 0xa9, 0x26, 0x34, 0x2f, 0x33, 0x9a,
+	0xb0, 0x7c, 0xa6, 0x0d, 0x28, 0x1d, 0x97, 0x4a, 0x1a, 0x89, 0xb7, 0x2d, 0x13, 0x5b, 0x26, 0xf6,
+	0xcc, 0x70, 0x7f, 0x99, 0x71, 0x0a, 0x46, 0x09, 0xe6, 0x7d, 0xc3, 0x3d, 0x8f, 0x94, 0x22, 0xa9,
+	0x26, 0x09, 0x93, 0x0a, 0x12, 0xca, 0xb9, 0x02, 0xdd, 0x00, 0x4f, 0x6f, 0x03, 0x19, 0xd0, 0xdc,
+	0x64, 0x84, 0x65, 0xc0, 0x7e, 0x78, 0x2a, 0x74, 0x94, 0x99, 0x97, 0x90, 0x94, 0xa0, 0x18, 0x14,
+	0xc6, 0x29, 0xd1, 0x67, 0xd4, 0x3b, 0xf1, 0xa5, 0xe2, 0x73, 0xb4, 0xe9, 0xcb, 0x26, 0xda, 0x50,
+	0x33, 0xd3, 0xa0, 0xc3, 0x60, 0xb4, 0x32, 0xee, 0x4f, 0xa2, 0x78, 0x49, 0xfd, 0xb1, 0x37, 0x5e,
+	0x5a, 0x36, 0xdd, 0x60, 0xed, 0x23, 0xe8, 0xe8, 0x4f, 0x80, 0x06, 0x0b, 0x08, 0xc6, 0xa8, 0x5b,
+	0xd0, 0x29, 0x84, 0xc1, 0x28, 0x18, 0xdf, 0x4f, 0xed, 0x6f, 0x1c, 0xa1, 0x01, 0xe5, 0x1c, 0x38,
+	0xa9, 0x04, 0x25, 0xb4, 0x14, 0x61, 0x67, 0x14, 0x8c, 0x7b, 0x69, 0xdf, 0x3e, 0xfc, 0x24, 0xe8,
+	0x51, 0x29, 0xf0, 0x17, 0xb4, 0xa7, 0x67, 0x8c, 0x81, 0xd6, 0x44, 0x51, 0x03, 0x04, 0xbe, 0x03,
+	0x33, 0x42, 0x16, 0xc4, 0x64, 0x0a, 0x74, 0x26, 0x73, 0x1e, 0xae, 0x8c, 0x82, 0x71, 0x7f, 0xb2,
+	0xed, 0xeb, 0xac, 0x83, 0xc6, 0x17, 0x2e, 0x68, 0xba, 0xeb, 0xbd, 0x29, 0x35, 0xf0, 0xd6, 0x3b,
+	0x3f, 0x36, 0x46, 0x7c, 0x8a, 0x06, 0x99, 0xd4, 0xe6, 0x26, 0x71, 0xd7, 0x26, 0xde, 0x5b, 0x9a,
+	0xf8, 0x4c, 0x6a, 0xe3, 0xe3, 0xae, 0x67, 0xd7, 0xbf, 0x41, 0x47, 0xbf, 0x3b, 0x08, 0xdd, 0x88,
+	0xf8, 0x00, 0xdd, 0xf3, 0x63, 0xb2, 0x59, 0xfb, 0x93, 0x61, 0xf3, 0xba, 0x52, 0xc4, 0xd5, 0x24,
+	0xae, 0xe7, 0x14, 0x1f, 0x39, 0x22, 0x6d, 0x50, 0xfc, 0x0a, 0xad, 0xd6, 0x55, 0xe8, 0xb0, 0x63,
+	0x4b, 0xd8, 0x5f, 0x5a, 0xc2, 0xa5, 0x98, 0x96, 0x39, 0x9c, 0xdb, 0x2d, 0x49, 0x1d, 0x8f, 0xdf,
+	0xa1, 0x81, 0x1f, 0xba, 0x4b, 0xe1, 0xbb, 0xf1, 0xec, 0xce, 0x0c, 0x67, 0x96, 0xbe, 0x4e, 0xd2,
+	0x3a, 0xe1, 0x43, 0xb4, 0xde, 0xee, 0x75, 0xd8, 0xbd, 0xbb, 0xb1, 0xfd, 0x56, 0x63, 0xf1, 0x63,
+	0xb4, 0xf6, 0x13, 0xc4, 0x55, 0x66, 0xc2, 0xd5, 0x51, 0x30, 0x1e, 0xa4, 0xfe, 0x14, 0xfd, 0xed,
+	0xa0, 0xcd, 0xff, 0xaf, 0xc4, 0xaf, 0xd1, 0xf0, 0x1b, 0x15, 0x39, 0x70, 0x42, 0x99, 0x11, 0x15,
+	0x90, 0xf6, 0xce, 0xda, 0x96, 0xf5, 0xd2, 0x1d, 0x47, 0x1c, 0x59, 0xc0, 0xb9, 0x4f, 0x6a, 0x19,
+	0xbf, 0x40, 0x0f, 0xbd, 0x59, 0xce, 0x4c, 0x2e, 0x40, 0x79, 0x9b, 0x5b, 0x1c, 0xec, 0xb4, 0x0f,
+	0x4e, 0x72, 0x8e, 0x37, 0x68, 0x77, 0xf1, 0x3a, 0x0e, 0x57, 0x8a, 0xd6, 0x4b, 0xe7, 0x9c, 0x5d,
+	0xeb, 0x7c, 0xd2, 0xbe, 0xf0, 0xd4, 0x13, 0xee, 0x05, 0x87, 0x68, 0xa7, 0x84, 0x82, 0x8b, 0xe2,
+	0x8a, 0xf0, 0x79, 0x41, 0xa7, 0x82, 0x11, 0x05, 0x53, 0x59, 0xd1, 0xdc, 0xa6, 0xed, 0xa5, 0x8f,
+	0xbc, 0x7c, 0xea, 0xd4, 0xd4, 0x89, 0xf8, 0x39, 0xda, 0x6a, 0x7c, 0x4d, 0x50, 0x16, 0xae, 0x59,
+	0xc7, 0x86, 0x17, 0x7c, 0x3e, 0x86, 0xdf, 0xa3, 0x2d, 0xe0, 0x9a, 0xdc, 0x1e, 0xe4, 0x83, 0x9b,
+	0x65, 0x6c, 0x6d, 0xcf, 0xc2, 0x08, 0x37, 0x80, 0xeb, 0xf6, 0x83, 0xe3, 0x03, 0xb4, 0x2f, 0xa4,
+	0x73, 0x95, 0x4a, 0xfe, 0x9a, 0x2f, 0xdb, 0x84, 0xe3, 0xe6, 0xdf, 0xa9, 0x2f, 0xea, 0x4f, 0xc1,
+	0x45, 0xf0, 0x75, 0xcd, 0x7e, 0x13, 0x5e, 0xfe, 0x0b, 0x00, 0x00, 0xff, 0xff, 0x96, 0x70, 0xc0,
+	0xed, 0xd2, 0x04, 0x00, 0x00,
 }

commit 714f1bf42f13957b47176ebc9340476cf3f017bf

accesslog: Fix status code parsing
Use absl::SimpleAtoi instead of StringUtil::atoill, as a string view
is not 0-terminated, as requiored by the latter. This fixes
incorrectly parsed status codes in the access log messages.

Fixes: b5f584ca ("istio: Update to 1.2.0")
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

cilium/accesslog.cc
@@ -164,7 +164,7 @@ void AccessLog::Entry::UpdateFromResponse(
     const Http::HeaderEntry *status_entry = headers.Status();
     if (status_entry) {
       uint64_t status;
-      if (StringUtil::atoull(status_entry->value().getStringView().data(), status, 10)) {
+      if (absl::SimpleAtoi(status_entry->value().getStringView(), &status)) {
         http_entry->set_status(status);
       }
     }

cilium_integration_test.cc
@@ -488,7 +488,7 @@ public:
     response->waitForEndStream();
 
     uint64_t status;
-    StringUtil::atoull(response->headers().Status()->value().getStringView().data(), status, 10);
+    absl::SimpleAtoi(response->headers().Status()->value().getStringView(), &status);
     EXPECT_EQ(403, status);
   }
 
@@ -499,7 +499,7 @@ public:
     auto response = sendRequestAndWaitForResponse(headers, 0, default_response_headers_, 0);
 
     uint64_t status;
-    StringUtil::atoull(response->headers().Status()->value().getStringView().data(), status, 10);
+    absl::SimpleAtoi(response->headers().Status()->value().getStringView(), &status);
     EXPECT_EQ(200, status);
   }
 
@@ -819,7 +819,7 @@ TEST_P(CiliumIntegrationTest, DuplicatePort) {
   response->waitForEndStream();
 
   uint64_t status;
-  StringUtil::atoull(response->headers().Status()->value().getStringView().data(), status, 10);
+  absl::SimpleAtoi(response->headers().Status()->value().getStringView(), &status);
   EXPECT_EQ(403, status);
 }
 