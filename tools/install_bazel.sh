b5a4dadb4c4fbbaee3f1eadef87cf29cc0f0e619
Jarno Rajahalme <jarno@covalent.io>
2020-11-25 00:00:15 -0800

    Makefile: More Bazel ARM64 support
    Version linkstamp path is architecture specific.
    
    Unify tools/install_bazel.sh now that Bazel releases are available for ARM64.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- tools/install_bazel.sh
+++ tools/install_bazel.sh
@@ -4,20 +4,18 @@ BAZEL_VERSION=$1
 
 echo "Checking if Bazel ${BAZEL_VERSION} needs to be installed..."
 if [[ $(command -v bazel) && "$(bazel version | grep 'label' | cut -d ' ' -f 3)" =~ ${BAZEL_VERSION} ]]; then
-  echo "Bazel ${BAZEL_VERSION} already installed, skipping fetch."
+    echo "Bazel ${BAZEL_VERSION} already installed, skipping fetch."
 else
-  if [ "$(uname -m)" = "aarch64" ]; then
-     echo "Install Bazel on arm64: ......"
-     sudo apt-get update && DEBIAN_FRONTEND=noninteractive sudo -E apt-get install -y curl unzip zip build-essential openjdk-11-jdk
-     sudo sh -c 'echo "deb https://download.opensuse.org/repositories/home:/mrostecki:/bazel/xUbuntu_20.04/ /" > /etc/apt/sources.list.d/bazel.list'
-     curl -L https://download.opensuse.org/repositories/home:/mrostecki:/bazel/xUbuntu_20.04/Release.key | sudo apt-key add -
-     sudo apt-get update && DEBIAN_FRONTEND=noninteractive sudo -E apt-get install -y bazel
-  else
-     echo "Install Bazel ..."
-     wget -nv https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
-     chmod +x bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
-     sudo -E ./bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
-     sudo -E mv /usr/local/bin/bazel /usr/bin
-     rm bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
-  fi
+    BAZEL=$(command -v bazel)
+    if [ -n "${BAZEL}" ] ; then
+	echo "Removing old Bazel version at ${BAZEL}"
+	sudo rm ${BAZEL}
+    else
+	BAZEL=/usr/local/bin/bazel
+    fi
+    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+    ARCH=$(uname -m) && [ "$ARCH" != "aarch64" ] || ARCH="arm64"
+    echo "Downloading bazel-${BAZEL_VERSION}-${OS}-${ARCH} to ${BAZEL}"
+    sudo curl -sfL https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-${OS}-${ARCH} -o ${BAZEL}
+    sudo chmod +x ${BAZEL}
 fi
