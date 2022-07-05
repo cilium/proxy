ea10d852b84262a4de00eaa3659399403507465d
Jarno Rajahalme <jarno@isovalent.com>
2022-07-05 15:06:23 +0300

    policy: Use upstream dst address only when L7 LB
    Add 'isL7LB()' predicate to always use the original destination address
    when not using L7 LB.
    
    The actual destination address may be different from the original
    destination address, but a pod's egress policy enforcement should always
    be done on the basis of the destination address after service load
    balancing, even when the Listener may forward to a different address.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- tests/bpf_metadata.cc
+++ tests/bpf_metadata.cc
@@ -92,7 +92,7 @@ bool TestConfig::getMetadata(Network::ConnectionSocket& socket) {
   }
 
   socket.addOption(std::make_shared<Cilium::SocketOption>(
-      policy, false, source_identity, is_ingress_, port,
+      policy, 0, source_identity, is_ingress_, false, port,
       std::move(pod_ip), nullptr, nullptr, nullptr, shared_from_this()));
 
   return true;
