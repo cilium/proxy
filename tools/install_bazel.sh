b9d4e6e1064a9fca5a8f78c1517009e2b531247b
Jarno Rajahalme <jarno@isovalent.com>
2023-02-05 21:00:48 +0200

    builder: Cross-compile with clang 15 on Ubuntu 20.04
    Bump builder back to Ubuntu 20.04 and install clang-15 to cross-compile
    with it instead of GCC. This has a few benefits:
    
    - Upstream Envoy builds with clang, so less incompatibilities due to GCC
      when updating
    - Binaries are compatible with Ubuntu 20.04 and newer without hacks with
      glibc
    
    This allowed removal of several GCC-related cross-compilation patches,
    but using clang-15 also required cherry-picking an upstream fix for
    boringssl as well.
    
    boringssl also required a fix (in an earlier commit) to be build
    compatible with legacy toolchains (without platforms). We should
    transition to platforms-based build soon, as upstream Envoy seems to be
    there already.
    
    Clang 15 complined about override not being marked as such on our tests,
    fixed.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- tools/install_bazel.sh
+++ tools/install_bazel.sh
@@ -2,7 +2,9 @@
 
 BAZEL_VERSION=$1
 if ! command -v sudo >/dev/null; then
-    exit 0
+    SUDO=
+else
+    SUDO=sudo
 fi
 
 echo "Checking if Bazel ${BAZEL_VERSION} needs to be installed..."
@@ -12,13 +14,13 @@ else
     BAZEL=$(command -v bazel)
     if [ -n "${BAZEL}" ] ; then
 	echo "Removing old Bazel version at ${BAZEL}"
-	sudo rm ${BAZEL}
+	${SUDO} rm ${BAZEL}
     else
 	BAZEL=/usr/local/bin/bazel
     fi
     OS=$(uname -s | tr '[:upper:]' '[:lower:]')
     ARCH=$(uname -m) && [ "$ARCH" != "aarch64" ] || ARCH="arm64"
     echo "Downloading bazel-${BAZEL_VERSION}-${OS}-${ARCH} to ${BAZEL}"
-    sudo curl -sfL https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-${OS}-${ARCH} -o ${BAZEL}
-    sudo chmod +x ${BAZEL}
+    ${SUDO} curl -sfL https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-${OS}-${ARCH} -o ${BAZEL}
+    ${SUDO} chmod +x ${BAZEL}
 fi
