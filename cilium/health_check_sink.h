df73ce09fdbaab26e40a0398e2c467a6793aa830
Jarno Rajahalme <jarno@isovalent.com>
2023-09-08 10:18:51 +0300

    api: Add cilium.health_check.event_sink.pipe extension
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

---
+++ cilium/health_check_sink.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "envoy/upstream/health_check_event_sink.h"
+
+#include "cilium/api/health_check_sink.pb.h"
+#include "cilium/api/health_check_sink.pb.validate.h"
+#include "cilium/uds_client.h"
+
+namespace Envoy {
+namespace Cilium {
+
+class HealthCheckEventPipeSinkFactory;
+
+class HealthCheckEventPipeSink : public Upstream::HealthCheckEventSink {
+public:
+  void log(envoy::data::core::v3::HealthCheckEvent event) override;
+
+protected:
+  friend class HealthCheckEventPipeSinkFactory;
+  friend class HealthCheckEventPipeSink_logTest_Test;
+  explicit HealthCheckEventPipeSink(const cilium::HealthCheckEventPipeSink& config);
+
+private:
+  static Thread::MutexBasicLockable udss_mutex;
+  static std::map<std::string, std::weak_ptr<UDSClient>> udss ABSL_GUARDED_BY(udss_mutex);
+
+  std::shared_ptr<UDSClient> uds_client_;
+};
+
+class HealthCheckEventPipeSinkFactory : public Upstream::HealthCheckEventSinkFactory {
+public:
+  HealthCheckEventPipeSinkFactory() = default;
+
+  Upstream::HealthCheckEventSinkPtr
+  createHealthCheckEventSink(const ProtobufWkt::Any& config,
+                             Server::Configuration::HealthCheckerFactoryContext& context) override;
+
+  std::string name() const override { return "cilium.health_check.event_sink.pipe"; }
+
+  ProtobufTypes::MessagePtr createEmptyConfigProto() override {
+    return ProtobufTypes::MessagePtr{new cilium::HealthCheckEventPipeSink()};
+  }
+};
+
+} // namespace Upstream
+} // namespace Envoy
---
+++ tests/health_check_sink_server.cc
@@ -0,0 +1,52 @@
+#include "tests/health_check_sink_server.h"
+
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <string>
+
+#include "source/common/common/lock_guard.h"
+#include "source/common/common/utility.h"
+
+#include "test/test_common/thread_factory_for_test.h"
+
+namespace Envoy {
+
+HealthCheckSinkServer::HealthCheckSinkServer(const std::string path) :
+    UDSServer(path, std::bind(&HealthCheckSinkServer::msgCallback, this, std::placeholders::_1)) {}
+
+HealthCheckSinkServer::~HealthCheckSinkServer() {}
+
+void HealthCheckSinkServer::clear() {
+  absl::MutexLock lock(&mutex_);
+  events_.clear();
+}
+
+absl::optional<envoy::data::core::v3::HealthCheckEvent>
+HealthCheckSinkServer::waitForEvent(std::chrono::milliseconds timeout) {
+  absl::MutexLock lock(&mutex_);
+  auto predicate = [this]() ABSL_SHARED_LOCKS_REQUIRED(mutex_) {
+    mutex_.AssertHeld();
+    return !events_.empty();
+  };
+  mutex_.AwaitWithTimeout(absl::Condition(&predicate), absl::Milliseconds(timeout.count()));
+  auto event = events_.front();
+  events_.pop_front();
+  return event;
+}
+
+void HealthCheckSinkServer::msgCallback(const std::string& data) {
+  envoy::data::core::v3::HealthCheckEvent event;
+  if (!event.ParseFromString(data)) {
+    ENVOY_LOG(warn, "Health check event parse failed!");
+  } else {
+    ENVOY_LOG(info, "Health check event: {}", event.DebugString());
+    absl::MutexLock lock(&mutex_);
+    events_.emplace_back(event);
+  }
+}
+
+} // namespace Envoy
