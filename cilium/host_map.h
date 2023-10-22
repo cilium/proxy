6c0b5f3fceb47b6c26c436d7a5e44e5f0e8e3e56
Tam Mach <tam.mach@cilium.io>
2023-10-22 22:49:25 +1100

    cilium: Adjust onConfigUpdate signature
    Relates: https://github.com/envoyproxy/envoy/pull/29439
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- cilium/host_map.h
+++ cilium/host_map.h
@@ -199,15 +199,16 @@ public:
   }
 
   // Config::SubscriptionCallbacks
-  void onConfigUpdate(const std::vector<Envoy::Config::DecodedResourceRef>& resources,
-                      const std::string& version_info) override;
-  void onConfigUpdate(const std::vector<Envoy::Config::DecodedResourceRef>& added_resources,
-                      const Protobuf::RepeatedPtrField<std::string>& removed_resources,
-                      const std::string& system_version_info) override {
+  absl::Status onConfigUpdate(const std::vector<Envoy::Config::DecodedResourceRef>& resources,
+                              const std::string& version_info) override;
+  absl::Status onConfigUpdate(const std::vector<Envoy::Config::DecodedResourceRef>& added_resources,
+                              const Protobuf::RepeatedPtrField<std::string>& removed_resources,
+                              const std::string& system_version_info) override {
     // NOT IMPLEMENTED YET.
     UNREFERENCED_PARAMETER(added_resources);
     UNREFERENCED_PARAMETER(removed_resources);
     UNREFERENCED_PARAMETER(system_version_info);
+    return absl::OkStatus();
   }
   void onConfigUpdateFailed(Envoy::Config::ConfigUpdateFailureReason,
                             const EnvoyException* e) override;
