7693ff9bc7b953366e510f130647da66065ef1e2
Tam Mach <tam.mach@cilium.io>
2024-07-22 15:11:33 +1000

    clang: Sync up setup_clang.sh with upstream
    This is to resolve the below issue with /rules_foreign_cc
    
    ```
    checking whether the C compiler works... no
    configure: error: in `/home/tammach/.cache/bazel/_bazel_tammach/c03a6fc3854531277731ed65d64acf86/sandbox/linux-sandbox/1461/execroot/cilium/bazel-out/k8-opt-exec-2B5CBBC6/bin/external/rules_foreign_cc/toolchains/pkgconfig.build_tmpdir':
    configure: error: C compiler cannot create executables
    See `config.log' for more details
    ```

--- bazel/setup_clang.sh
+++ bazel/setup_clang.sh
@@ -13,21 +13,18 @@ BINDIR="$("${LLVM_PREFIX}"/bin/llvm-config --bindir)"
 PATH="${BINDIR}:${PATH}"
 export PATH
 
-RT_LIBRARY_PATH="$(dirname "$(find "$(llvm-config --libdir)" -name libclang_rt.ubsan_standalone_cxx-x86_64.a | head -1)")"
+LLVM_VERSION="$(llvm-config --version)"
+LLVM_LIBDIR="$(llvm-config --libdir)"
+LLVM_TARGET="$(llvm-config --host-target)"
+
+RT_LIBRARY_PATH="${LLVM_LIBDIR}/clang/${LLVM_VERSION}/lib/${LLVM_TARGET}"
 
 echo "# Generated file, do not edit. If you want to disable clang, just delete this file.
-build:clang --action_env='PATH=${PATH}'
-build:clang --action_env=CC=${BINDIR}/clang
-build:clang --action_env=CXX=${BINDIR}/clang++
-build:clang --action_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config'
+build:clang --action_env='PATH=${PATH}' --host_action_env='PATH=${PATH}'
+build:clang --action_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config' --host_action_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config'
 build:clang --repo_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config'
 build:clang --linkopt='-L$(llvm-config --libdir)'
 build:clang --linkopt='-Wl,-rpath,$(llvm-config --libdir)'
 
-build:clang-asan --action_env=ENVOY_UBSAN_VPTR=1
-build:clang-asan --copt=-fsanitize=vptr,function
-build:clang-asan --linkopt=-fsanitize=vptr,function
 build:clang-asan --linkopt='-L${RT_LIBRARY_PATH}'
-build:clang-asan --linkopt=-l:libclang_rt.ubsan_standalone-x86_64.a
-build:clang-asan --linkopt=-l:libclang_rt.ubsan_standalone_cxx-x86_64.a
-" > "${BAZELRC_FILE}"
+" >"${BAZELRC_FILE}"
