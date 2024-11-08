56316c0a012b32e87926b35f7d621dc09cf1ea30
Jarno Rajahalme <jarno@isovalent.com>
2024-11-08 12:26:47 +0100

    tls_wrapper: Use raw socket with non-TLS policy
    Cilium tls_wrapper should allow raw socket to be used if policy allows
    without TLS context.
    
    Also check for SNI when getting TLS context so that the one matching the
    SNI is used if multiple are available.
    
    Add policy tests validating the policy functionality.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- cilium/tls_wrapper.h
+++ cilium/tls_wrapper.h
@@ -2,25 +2,9 @@
 
 #include "envoy/registry/registry.h"
 #include "envoy/server/transport_socket_config.h"
-#include "envoy/stats/scope.h"
-#include "envoy/stats/stats_macros.h"
 
 namespace Envoy {
 namespace Cilium {
-
-// clang-format off
-#define ALL_SSL_SOCKET_FACTORY_STATS(COUNTER)                                 \
-  COUNTER(ssl_context_update_by_sds)                                          \
-  COUNTER(upstream_context_secrets_not_ready)                                 \
-  COUNTER(downstream_context_secrets_not_ready)
-// clang-format on
-
-/**
- * Wrapper struct for SSL socket factory stats. @see stats_macros.h
- */
-struct SslSocketFactoryStats {
-  ALL_SSL_SOCKET_FACTORY_STATS(GENERATE_COUNTER_STRUCT)
-};
 
 /**
  * Config registration for the Cilium TLS wrapper transport socket factory.
