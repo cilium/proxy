994b4415aea97a566041766b86f3b5312155e65f
Tam Mach <sayboras@yahoo.com>
2024-12-02 22:01:28 +1100

    Revert "debug: Assert policy destruction happens in the main thread"
    This reverts commit 17c48a77763d32c18d3c2b04b587949a2da39e4a.

--- cilium/secret_watcher.cc
+++ cilium/secret_watcher.cc
@@ -34,10 +34,7 @@ SecretWatcher::SecretWatcher(const NetworkPolicyMap& parent, const std::string& 
       secret_provider_(secretProvider(parent.transportFactoryContext(), sds_name)),
       update_secret_(readAndWatchSecret()) {}
 
-SecretWatcher::~SecretWatcher() {
-  ASSERT_IS_MAIN_OR_TEST_THREAD();
-  delete load();
-}
+SecretWatcher::~SecretWatcher() { delete load(); }
 
 Envoy::Common::CallbackHandlePtr SecretWatcher::readAndWatchSecret() {
   store();
