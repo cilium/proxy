85584ec707511ec8c217c0089d3153d36679b68b
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-12-20 17:20:54 +0100

    so_reuseport socket option
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- cilium/socket_option.h
+++ cilium/socket_option.h
@@ -108,21 +108,6 @@ public:
       }
     }
 
-    uint32_t one = 1;
-
-    if (identity_ != 0) {
-      // Set SO_REUSEPORT socket option for forwarded client connections.
-      // The same option on the listener socket is controlled via the Envoy Listener option
-      // enable_reuse_port.
-      ENVOY_LOG(trace, "Set socket ({}) option SO_REUSEPORT", socket.ioHandle().fdDoNotUse());
-      status = socket.setSocketOption(SOL_SOCKET, SO_REUSEPORT, &one, sizeof(one));
-      if (status.return_value_ < 0) {
-        ENVOY_LOG(critical, "Failed to set socket option SO_REUSEPORT: {}",
-                  Envoy::errorDetails(status.errno_));
-        return false;
-      }
-    }
-
     ENVOY_LOG(trace,
               "Set socket ({}) option SO_MARK to {:x} (magic mark: {:x}, id: "
               "{}, cluster: {}), src: {}",
@@ -285,6 +270,47 @@ public:
                   Envoy::errorDetails(status.errno_));
         return false;
       }
+    }
+
+    return true;
+  }
+
+  void hashKey([[maybe_unused]] std::vector<uint8_t>& key) const override {}
+
+  bool isSupported() const override { return true; }
+};
+
+class ReusePortSocketOption : public Network::Socket::Option,
+                              public Logger::Loggable<Logger::Id::filter> {
+public:
+  ReusePortSocketOption() { ENVOY_LOG(debug, "Cilium ReusePortSocketOption()"); }
+
+  absl::optional<Network::Socket::Option::Details>
+  getOptionDetails(const Network::Socket&,
+                   envoy::config::core::v3::SocketOption::SocketState) const override {
+    return absl::nullopt;
+  }
+
+  bool setOption(Network::Socket& socket,
+                 envoy::config::core::v3::SocketOption::SocketState state) const override {
+    // Only set the option once per socket
+    if (state != envoy::config::core::v3::SocketOption::STATE_PREBIND) {
+      ENVOY_LOG(trace, "Skipping setting socket ({}) option SO_REUSEPORT, state != STATE_PREBIND",
+                socket.ioHandle().fdDoNotUse());
+      return true;
+    }
+
+    uint32_t one = 1;
+
+    // Set SO_REUSEPORT socket option for forwarded client connections.
+    // The same option on the listener socket is controlled via the Envoy Listener option
+    // enable_reuse_port.
+    ENVOY_LOG(trace, "Set socket ({}) option SO_REUSEPORT", socket.ioHandle().fdDoNotUse());
+    auto status = socket.setSocketOption(SOL_SOCKET, SO_REUSEPORT, &one, sizeof(one));
+    if (status.return_value_ < 0) {
+      ENVOY_LOG(critical, "Failed to set socket option SO_REUSEPORT: {}",
+                Envoy::errorDetails(status.errno_));
+      return false;
     }
 
     return true;
