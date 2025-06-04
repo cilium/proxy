218d82999e024866187d8928c748af64db2b94d8
Jarno Rajahalme <jarno@isovalent.com>
2025-06-04 20:32:57 +0200

    l7lb: Pass original source address in option for east/west
    Source address socket option uses 'hashKey' function to choose the
    upstream connection pool. 'hashKey' has no access to the socket (which
    has not been created yet), so it needs access to the original source
    address which can be either IPv4 or IPv6 to be able to pass in the
    original source address and port in the resulting hash.
    
    This fixes the issue with east/west l7lb that caused upstream connection
    pool being shared between all local endpoints with the same security
    identity, while using the original source address. This manifested as
    connection pool attempting to create more than one connection with the
    same 5-tuple, which failed and resulted in '503' responses in HTTP.
    
    Fixes: #65
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- cilium/socket_option_source_address.cc
+++ cilium/socket_option_source_address.cc
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 
 #include <cstdint>
+#include <memory>
 #include <utility>
 #include <vector>
 
@@ -56,6 +57,12 @@ bool SourceAddressSocketOption::setOption(
   }
 
   Network::Address::InstanceConstSharedPtr source_address = original_source_address_;
+
+  // Do not use source address of wrong IP version
+  if (source_address && source_address->ip() && source_address->ip()->version() != *ip_version) {
+    source_address = nullptr;
+  }
+
   if (!source_address && (ipv4_source_address_ || ipv6_source_address_)) {
     // Select source address based on the socket address family
     source_address = ipv6_source_address_;
