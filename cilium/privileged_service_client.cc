cilium/proxy

commit 49b6a7c9c075b587f26255e73eda3fa08506b3e4

cilium: Use cilium-envoy-starter to run Envoy without privileges
Introduce a `cilium-envoy-starter` that forks and execs `cilium-envoy`
after having dropped all privileges.

Perform bpf map and privileged socket option operations in
`cilium-envoy-starter`, as requested by Cilium filters running in
`cilium-envoy` via a pipe between the two. Currently the protocol over
this pipe supports only the following operations:

- dump capabilities cilium-envoy-starter is running with
- open bpf maps
- perform bpf map lookups
- set socket options

cilium-envoy-starter fails to start if it does not have adequate
privileges needed for the above operations.

cilium-envoy now exits if it is running with any privileges when Cilium
filters are first configured.

Implementation detail:

- libcap is not used, as it would be hard to cross-compile for both arm64
  and amd64. Fortunately we only need to drop capabilities, so using the
  syscall interface is not complicated.

- Patch Envoy to support setting socket options from listener filters and
  use them to set privileged options for the listener.

- Remove the unused support for creating and manipulating bpf maps.

- Remove setrlimit call that is only needed if the process is creating
  bpf maps.

- Minor cleanup in socket option implementation to move unused fields
  from SocketMarkOption class to SocketOption where they are used.

Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

cilium/BUILD
@@ -175,6 +175,7 @@ envoy_cc_library(
     ],
     repository = "@envoy",
     deps = [
+        "privileged_service_client_lib",
         "@envoy//source/common/common:logger_lib",
         "@envoy//source/common/common:utility_lib",
     ],
@@ -296,5 +297,17 @@ envoy_cc_library(
         "@envoy//source/common/common:logger_lib",
         "@envoy//source/common/network:address_lib",
         "@envoy//source/common/router:config_utility_lib",
+    ],
+)
+
+envoy_cc_library(
+    name = "privileged_service_client_lib",
+    hdrs = ["privileged_service_client.h"],
+    srcs = ["privileged_service_client.cc"],
+    repository = "@envoy",
+    deps = [
+        "//starter:privileged_service_protocol",
+        "@envoy//envoy/api:os_sys_calls_interface",
+        "@envoy//source/common/singleton:threadsafe_singleton",
     ],
 )

cilium/privileged_service_client.cc
@@ -1,0 +1,131 @@
+#if !defined(__linux__)
+#error "Linux platform file is part of non-Linux build."
+#endif
+
+#include "cilium/privileged_service_client.h"
+
+namespace Envoy {
+namespace Cilium {
+namespace PrivilegedService {
+
+ProtocolClient::ProtocolClient() :
+  Protocol(CILIUM_PRIVILEGED_SERVICE_FD),
+  call_mutex_(PTHREAD_MUTEX_INITIALIZER),
+  seq_(0) {
+  RELEASE_ASSERT(get_capabilities(CAP_EFFECTIVE) == 0 && get_capabilities(CAP_PERMITTED) == 0,
+		 "cilium-envoy running with privileges, exiting");
+
+  if (!check_privileged_service()) {
+    // No Cilium privileged service detected
+    close();
+  }
+
+  // Validate that direct SO_MARK is now prohibited
+  int sockfd = ::socket(AF_INET, SOCK_STREAM, 0);
+  RELEASE_ASSERT(sockfd >= 0, "socket failed");
+
+  uint32_t mark = 12345;
+  int rc = ::setsockopt(sockfd, SOL_SOCKET, SO_MARK, &mark, sizeof(mark));
+  RELEASE_ASSERT(rc == -1, "setsockopt");
+  RELEASE_ASSERT(errno == EPERM, "setsockopt");
+
+  ::close(sockfd);
+}
+
+ssize_t ProtocolClient::transact(MessageHeader& req, size_t req_len, const void *data, size_t data_len, int *fd, Response& resp, void *buf, size_t bufsize, bool assert) {
+  uint32_t expected_response_type = resp.hdr_.msg_type_;
+
+  // Serialize calls to cilium privileged service
+  int rc = pthread_mutex_lock(&call_mutex_);
+  RELEASE_ASSERT(rc == 0, "pthread_mutex_lock");
+
+  req.msg_seq_ = ++seq_;
+  ssize_t size = send_fd_msg(&req, req_len, data, data_len, *fd);
+  if (!assert && size_t(size) != req_len + data_len) {
+    goto out;
+  }
+  RELEASE_ASSERT(size != 0, "Cilium privileged service closed pipe");
+  RELEASE_ASSERT(size > 0, "Cilium privileged service send failed");
+
+  size = recv_fd_msg(&resp, sizeof(resp), buf, bufsize, fd);
+  if (!assert && size_t(size) < sizeof(resp)) {
+    goto out;
+  }
+  RELEASE_ASSERT(size != 0, "Cilium privileged service closed pipe");
+  RELEASE_ASSERT(size < 0 || size_t(size) >= sizeof(resp), "Cilium privileged service truncated response");
+  RELEASE_ASSERT(resp.hdr_.msg_seq_ == req.msg_seq_, "Cilium privileged service response out of sequence");
+  RELEASE_ASSERT(resp.hdr_.msg_type_ == expected_response_type,
+		 "Cilium privileged service unexpected response type");
+
+ out:
+  rc = pthread_mutex_unlock(&call_mutex_);
+  RELEASE_ASSERT(rc == 0, "pthread_mutex_unlock");
+  return size;
+}
+
+bool ProtocolClient::check_privileged_service() {
+  // Dump the effective capabilities of the privileged service process
+  DumpRequest req;
+  Response resp;
+  uint8_t buf[1024];
+  int fd = -1;
+  ssize_t size = transact(req.hdr_, sizeof(req), nullptr, 0, &fd, resp, buf, sizeof(buf), false);
+  if (size < ssize_t(sizeof(resp))) {
+    return false;
+  }
+  std::string str(reinterpret_cast<char *>(buf), size - sizeof(resp));
+  ENVOY_LOG_MISC(debug, "Cilium privileged service detected with following capabilities: {}", str);
+  return true;
+}
+
+Envoy::Api::SysCallIntResult ProtocolClient::bpf_open(const char *path) {
+  if (!have_cilium_privileged_service()) {
+      return {-1, EPERM};
+  }
+
+  BpfOpenRequest req;
+  Response resp;
+  size_t path_len = strlen(path);
+  RELEASE_ASSERT(path_len <= PATH_MAX, "bpf open path too long");
+  int fd = -1;
+  ssize_t size = transact(req.hdr_, sizeof(req), path, path_len, &fd, resp);
+  RELEASE_ASSERT(size == ssize_t(sizeof(resp)), "invalid received response size");
+  if (resp.return_value_ == INT_MAX) {
+    resp.return_value_ = fd;
+  }
+  return Envoy::Api::SysCallIntResult{resp.return_value_, resp.errno_};
+}
+
+Envoy::Api::SysCallIntResult ProtocolClient::bpf_lookup(int fd, const void *key,
+							uint32_t key_size, void* value,
+							uint32_t value_size) {
+  if (!have_cilium_privileged_service()) {
+      return {-1, EPERM};
+  }
+
+  BpfLookupRequest req(value_size);
+  Response resp;
+  ssize_t size = transact(req.hdr_, sizeof(req), key, key_size, &fd, resp, value, value_size);
+  RELEASE_ASSERT((size == ssize_t(sizeof(resp)) && resp.return_value_ == -1)
+                 || size == ssize_t(sizeof(resp) + value_size),
+                 "invalid received bpf lookup value size");
+  return Envoy::Api::SysCallIntResult{resp.return_value_, resp.errno_};
+}
+
+Envoy::Api::SysCallIntResult ProtocolClient::setsockopt(int sockfd, int level, int optname,
+							const void *optval,
+							socklen_t optlen) {
+  if (!have_cilium_privileged_service()) {
+      return {-1, EPERM};
+  }
+
+  SetSockOptRequest req(level, optname, optval, optlen);
+  Response resp;
+  ssize_t size = transact(req.hdr_, sizeof(req), nullptr, 0, &sockfd, resp);
+  RELEASE_ASSERT(size == ssize_t(sizeof(resp)), "invalid received response size");
+  return Envoy::Api::SysCallIntResult{resp.return_value_, resp.errno_};
+}
+
+} // namespace PrivilegedService
+} // namespace Cilium
+} // namespace Envoy

cilium/privileged_service_client.h
@@ -1,0 +1,55 @@
+#pragma once
+
+#if !defined(__linux__)
+#error "Linux platform file is part of non-Linux build."
+#endif
+
+#include "source/common/common/assert.h"
+#include "source/common/singleton/threadsafe_singleton.h"
+
+#include "starter/privileged_service_protocol.h"
+
+#include "envoy/api/os_sys_calls_common.h"
+
+namespace Envoy {
+namespace Cilium {
+
+class Bpf;
+class SocketMarkOption;
+
+namespace PrivilegedService {
+
+// ProtocolClient implements the client logic for communicating with the privileged service.
+class ProtocolClient : public Protocol {
+public:
+  ProtocolClient();
+
+  // allow access to the classes that need it
+  friend class Envoy::Cilium::Bpf;
+  friend class Envoy::Cilium::SocketMarkOption;
+
+protected:
+  // Read-only bpf syscalls
+  Envoy::Api::SysCallIntResult bpf_open(const char *path);
+  Envoy::Api::SysCallIntResult bpf_lookup(int fd, const void *key, uint32_t key_size, void* value,
+					  uint32_t value_size);
+
+  // Set a socket option
+  Envoy::Api::SysCallIntResult setsockopt(int sockfd, int level, int optname, const void *optval,
+					  socklen_t optlen);
+
+private:
+  bool check_privileged_service();
+  bool have_cilium_privileged_service() const { return is_open(); }
+
+  ssize_t transact(MessageHeader& req, size_t req_len, const void *data, size_t datalen, int *fd, Response& resp, void *buf = nullptr, size_t bufsize = 0, bool assert = true);
+
+  pthread_mutex_t call_mutex_;
+  uint32_t seq_;
+};
+
+using Singleton = Envoy::ThreadSafeSingleton<ProtocolClient>;
+
+} // namespace PrivilegedService
+} // namespace Cilium
+} // namespace Envoy

starter/privileged_service_protocol.h
@@ -1,0 +1,158 @@
+#pragma once
+
+#if !defined(__linux__)
+#error "Linux platform file is part of non-Linux build."
+#endif
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <linux/capability.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+// Use Envoy version of this if defined, otherwise roll a simple stderr one without further
+// dependencies
+#ifndef RELEASE_ASSERT
+#define _ASSERT_IMPL(CONDITION, CONDITION_STR, DETAILS)			\
+  do {									\
+    if (!(CONDITION)) {							\
+      fprintf(stderr, "assert failure: %s. Details: %s", CONDITION_STR, (DETAILS)); \
+      ::abort();							\
+    }									\
+  } while (false)
+#define RELEASE_ASSERT(X, DETAILS) _ASSERT_IMPL(X, #X, DETAILS)
+#endif
+
+// Kernel headers may miss CAP_BPF added in Linux 5.8
+#ifndef CAP_BPF
+#define CAP_BPF                 39
+#endif
+
+#ifndef _SYS_CAPABILITY_H
+// These are normally defined in <sys/capability.h> added in libcap-dev package.  Define these here
+// to avoid that dependency due to complications in cross-compilation for Intel/Arm.
+typedef enum {
+    CAP_EFFECTIVE = 0,                 /* Specifies the effective flag */
+    CAP_PERMITTED = 1,                 /* Specifies the permitted flag */
+    CAP_INHERITABLE = 2                /* Specifies the inheritable flag */
+} cap_flag_t;
+#endif
+
+namespace Envoy {
+namespace Cilium {
+namespace PrivilegedService {
+
+uint64_t get_capabilities(cap_flag_t kind);
+size_t dump_capabilities(cap_flag_t kind, char *buf, size_t buf_size);
+
+#define CILIUM_PRIVILEGED_SERVICE_FD 3
+
+// Communication with the privileged service is performed with a simple message protocol over a Unix
+// domain socket pair using the SOCK_SEQPACKET type, preserving record boundaries without explicitly
+// encoding a length field.
+// Each message starts with a 4-byte type and a 4-byte sequence number, both in the host byte order,
+// which are followed by message type specific variable length data.
+
+// Supported message types
+typedef enum {
+  TYPE_RESPONSE = 1,
+  TYPE_DUMP_REQUEST = 2,
+  TYPE_BPF_OPEN_REQUEST = 3,
+  TYPE_BPF_LOOKUP_REQUEST = 4,
+  TYPE_SETSOCKOPT32_REQUEST = 5,
+} MessageType;
+
+// Common message header
+// Note that C++ inheritance can not be used as all the data members need to be on the same struct
+// definition. This means that we must contain the MessageHeader as the first member of each message
+// definition.
+struct MessageHeader {
+  uint32_t msg_type_ = 0;
+  uint32_t msg_seq_ = 0; // reflected in response
+
+  MessageHeader() {}
+  MessageHeader(MessageType t) : msg_type_(t) {}
+};
+
+// Response passes the return value and errno code from the system call, followed by optional
+// data depending on the request type. Note that file descriptor return value is passed using the
+// message control channel (ref. man 2 recvmsg).
+struct Response {
+  Response() : hdr_(TYPE_RESPONSE) {}
+
+  struct MessageHeader hdr_;
+  int return_value_;
+  int errno_;
+  uint8_t data_[];
+};
+
+
+// Dump requests consists only of the message header, but with the TYPE_DUMP_REQUEST.
+// Response contains the effective capabilitites in a string form.
+struct DumpRequest {
+  DumpRequest() : hdr_(TYPE_DUMP_REQUEST) {}
+
+  struct MessageHeader hdr_;
+};
+
+// BpfOpenRequest has a variable length path after the message header.
+// Path need not be 0-terminated.
+// Response must be a SyscallResponse. The file descriptor is returned in the message control
+// channel (see man 2 recvmsg).
+struct BpfOpenRequest {
+  BpfOpenRequest() : hdr_(TYPE_BPF_OPEN_REQUEST) {}
+
+  struct MessageHeader hdr_;
+  char path_[];
+};
+
+// BpfLookupRequest passes the expected value size and a variable length key.
+// Key size is not explicitly passed, as it is deduced from the message length.
+// In a successful case the response contains the value returned by the bpf map lookup.
+struct BpfLookupRequest {
+  BpfLookupRequest(uint32_t value_size) : hdr_(TYPE_BPF_LOOKUP_REQUEST), value_size_(value_size) {}
+
+  struct MessageHeader hdr_;
+  uint32_t value_size_;
+  uint8_t key_[];
+};
+
+// SetSockOptRequest only supports setting 4-byte options.
+// Response is SyscallResponse.
+struct SetSockOptRequest {
+  SetSockOptRequest(int level, int optname, const void *optval, socklen_t optlen)
+    : hdr_(TYPE_SETSOCKOPT32_REQUEST), level_(level), optname_(optname) {
+    RELEASE_ASSERT(optlen == sizeof(uint32_t), "optlen must be 4 bytes");
+    memcpy(&optval_, optval, optlen);
+  }
+
+  struct MessageHeader hdr_;
+  int level_;
+  int optname_;
+  uint32_t optval_;
+};
+
+// Protocol implements the logic for communicating with the privileged service.
+class Protocol {
+public:
+  Protocol(int fd);
+  ~Protocol();
+
+  void close();
+  bool is_open() const { return fd_ != -1; }
+
+  ssize_t send_fd_msg(const void *header, ssize_t headerlen,
+		      const void *data = nullptr, ssize_t datalen = 0, int fd = -1);
+  ssize_t recv_fd_msg(const void *header, ssize_t headersize,
+		      const void *data = nullptr, ssize_t datasize = 0, int *fd = nullptr);
+
+protected:
+  int fd_;
+};
+
+} // namespace PrivilegedService
+} // namespace Cilium
+} // namespace Envoy

BUILD
@@ -14,6 +14,13 @@ exports_files([
     "linux/type_mapper.h",
 ])
 
+cc_binary(
+    name = "cilium-envoy-starter",
+    deps = [
+        "//starter:main_entry_lib",
+    ],
+)
+
 envoy_cc_binary(
     name = "cilium-envoy",
     repository = "@envoy",

starter/BUILD
@@ -1,0 +1,26 @@
+licenses(["notice"])  # Apache 2
+
+cc_library(
+    name = "main_entry_lib",
+    srcs = ["main.cc"],
+    deps = [
+        "privileged_service_server_lib",
+    ],
+    visibility = ["//visibility:public"],
+)
+
+cc_library(
+    name = "privileged_service_protocol",
+    srcs = ["privileged_service_protocol.cc"],
+    hdrs = ["privileged_service_protocol.h"],
+    visibility = ["//visibility:public"],
+)
+
+cc_library(
+    name = "privileged_service_server_lib",
+    hdrs = ["privileged_service_server.h"],
+    srcs = ["privileged_service_server.cc"],
+    deps = [
+        "privileged_service_protocol",
+    ],
+)

.gitignore
@@ -12,6 +12,7 @@
 /bazel-*
 
 /cilium-envoy
+/cilium-envoy-starter
 /Dockerfile.istio_proxy
 /Dockerfile.builder-refresh
 

commit f99121ddcab1b91ee7e5102bfe703dd80c24bd8e

cilium: Reduce locking in privileged service client
Change the calls to "privileged services" to only take the `calls_mutex_`
for receiving responses.

Set up a `Waiter` for each caller so that the active receiver can pass
responses to them. The active receiver stops receiving as soon as it gets
its own response, and passes the receiver role to another "waiter".

Any changes to this code should be tested with a performance tool
maximally loading all the available worker threads (e.g., `wrk`).

This partially addresses the performance regression introduced in #315.

Fixes: #315
Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

cilium/BUILD
@@ -359,6 +359,7 @@ envoy_cc_library(
     deps = [
         "//starter:privileged_service_protocol",
         "@envoy//envoy/api:os_sys_calls_interface",
+        "@envoy//source/common/common:thread_lib",
         "@envoy//source/common/singleton:threadsafe_singleton",
     ],
 )

cilium/privileged_service_client.cc
@@ -7,7 +7,6 @@
 #include <asm-generic/socket.h>
 #include <linux/capability.h>
 #include <linux/limits.h>
-#include <pthread.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -30,8 +29,7 @@ namespace Envoy {
 namespace Cilium {
 namespace PrivilegedService {
 
-ProtocolClient::ProtocolClient()
-    : Protocol(CILIUM_PRIVILEGED_SERVICE_FD), call_mutex_(PTHREAD_MUTEX_INITIALIZER), seq_(0) {
+ProtocolClient::ProtocolClient() : Protocol(CILIUM_PRIVILEGED_SERVICE_FD), seq_(0) {
   // Check that the Envoy process isn't running with privileges.
   // The only exception is CAP_NET_BIND_SERVICE (if explicitly excluded from being dropped).
   RELEASE_ASSERT((getCapabilities(CAP_EFFECTIVE) & ~(1UL << CAP_NET_BIND_SERVICE)) == 0 &&
@@ -39,6 +37,7 @@ ProtocolClient::ProtocolClient()
                  "cilium-envoy running with privileges, exiting");
 
   if (!checkPrivilegedService()) {
+    ENVOY_LOG(warn, "Cilium privileged service not present");
     // No Cilium privileged service detected
     close();
   }
@@ -57,47 +56,98 @@ ProtocolClient::ProtocolClient()
 
 ssize_t ProtocolClient::transact(MessageHeader& req, size_t req_len, const void* data,
                                  size_t data_len, int* fd, Response& resp, void* buf,
-                                 size_t bufsize, bool assert) {
-  uint32_t expected_response_type = resp.hdr_.msg_type_;
+                                 size_t buf_size) {
+  RELEASE_ASSERT(buf_size <= RESPONSE_BUF_SIZE, "ProtocolClient::transact: invalid bufsize");
+  uint32_t seq;
 
-  // Serialize calls to cilium privileged service
-  int rc = pthread_mutex_lock(&call_mutex_);
-  RELEASE_ASSERT(rc == 0, "pthread_mutex_lock");
+  // get next atomic sequence number
+  do {
+    seq = ++seq_;
+  } while (seq == 0); // zero is reserved for "no response"
+  req.msg_seq_ = seq;
 
-  req.msg_seq_ = ++seq_;
+  // Set up a waiter in the stack before we send anything so that the waiter exists as soon as it is
+  // possible for a concurrent receiver to receive the response.
+  Waiter waiter;
+  waiters_.insert(seq, &waiter);
+
+  // send message after a waiter has been established.
   ssize_t size = sendFdMsg(&req, req_len, data, data_len, *fd);
-  if (!assert && size_t(size) != req_len + data_len) {
-    goto out;
+  if (size > 0) {
+    if (size_t(size) == req_len + data_len) {
+      // Request was sent completely, receive the response
+      size = waiterReceive(waiter, seq);
+    } else {
+      // send was truncated, return 0 to indicate failure
+      size = 0;
+    }
   }
-  RELEASE_ASSERT(size != 0, "Cilium privileged service closed pipe");
-  RELEASE_ASSERT(size > 0, "Cilium privileged service send failed");
 
-  size = recvFdMsg(&resp, sizeof(resp), buf, bufsize, fd);
-  if (!assert && size_t(size) < sizeof(resp)) {
-    goto out;
+  // waiter must be removed on all return paths. This is the only one.
+  waiters_.remove(seq);
+  return size > 0 ? waiter.getResponse(resp, buf, buf_size, fd) : size;
+}
+
+ssize_t ProtocolClient::waiterReceive(Waiter& waiter, uint32_t seq) noexcept {
+  while (true) {
+    // Try become a receiver by taking the call_mutex_.
+    if (call_mutex_.tryLock()) {
+      // Check if our response is already in, otherwise we could enter the receive loop and never
+      // receive our response.
+      ssize_t size = waiters_.setReceiverActiveIfNoResponse(waiter, seq);
+      if (size == 0) {
+        // Receive until we have a response or an error
+        while (true) {
+          size = waiter.recvFdMsg(*this);
+          RELEASE_ASSERT(size != 0, "Cilium privileged service closed pipe");
+          if (size < 0) {
+            // privileged service failed
+            ENVOY_LOG(debug, "privileged service failed with {} (errno {})", size, errno);
+            break;
+          }
+          // Must have enough data to decode the response header
+          RELEASE_ASSERT(size_t(size) >= sizeof(Response),
+                         "Cilium privileged service truncated response");
+          RELEASE_ASSERT(waiter.msgType() == MessageType::TypeResponse,
+                         "Cilium privileged service unexpected response type");
+
+          // Is the response for us?
+          if (waiter.seq() == seq) {
+            break;
+          }
+          // The response is for one of the waiters, pass it on
+          waiters_.wakeUp(waiter);
+        }
+      }
+
+      // Receive loop ended or not entered in the first place.
+      call_mutex_.unlock();
+      // Pass receiver duties to one of the other waiters, if any.
+      waiters_.signalReceiverOpen();
+      return size;
+    }
+
+    // tryLock failed, call_mutex_ not held.
+    // There already is an active receiver, wait for a response from it.
+    ssize_t size = waiters_.wait(waiter, seq);
+    if (size != 0) {
+      return size;
+    }
+    // Waiter woken up without a response.
+    // Loop back to try to become the receiver.
   }
-  RELEASE_ASSERT(size != 0, "Cilium privileged service closed pipe");
-  RELEASE_ASSERT(size < 0 || size_t(size) >= sizeof(resp),
-                 "Cilium privileged service truncated response");
-  RELEASE_ASSERT(resp.hdr_.msg_seq_ == req.msg_seq_,
-                 "Cilium privileged service response out of sequence");
-  RELEASE_ASSERT(resp.hdr_.msg_type_ == expected_response_type,
-                 "Cilium privileged service unexpected response type");
-
-out:
-  rc = pthread_mutex_unlock(&call_mutex_);
-  RELEASE_ASSERT(rc == 0, "pthread_mutex_unlock");
-  return size;
 }
 
 bool ProtocolClient::checkPrivilegedService() {
   // Dump the effective capabilities of the privileged service process
   DumpRequest req;
   Response resp;
-  uint8_t buf[1024];
+  uint8_t buf[RESPONSE_BUF_SIZE];
   int fd = -1;
-  ssize_t size = transact(req.hdr_, sizeof(req), nullptr, 0, &fd, resp, buf, sizeof(buf), false);
+
+  ssize_t size = transact(req.hdr_, sizeof(req), nullptr, 0, &fd, resp, buf, sizeof(buf));
   if (size < ssize_t(sizeof(resp))) {
+    ENVOY_LOG_MISC(warn, "Cilium privileged service detection failed with return code: {}", size);
     return false;
   }
   std::string str(reinterpret_cast<char*>(buf), size - sizeof(resp));

cilium/privileged_service_client.h
@@ -7,13 +7,21 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
+#include <atomic>
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
 
 #include "envoy/api/os_sys_calls_common.h"
 
+#include "source/common/common/assert.h"
+#include "source/common/common/lock_guard.h"
+#include "source/common/common/logger.h"
+#include "source/common/common/thread.h"
 #include "source/common/singleton/threadsafe_singleton.h"
 
+#include "absl/base/thread_annotations.h"
+#include "absl/container/flat_hash_map.h"
 #include "starter/privileged_service_protocol.h"
 
 namespace Envoy {
@@ -24,8 +32,10 @@ class SocketMarkOption;
 
 namespace PrivilegedService {
 
+#define RESPONSE_BUF_SIZE 1024
+
 // ProtocolClient implements the client logic for communicating with the privileged service.
-class ProtocolClient : public Protocol {
+class ProtocolClient : public Protocol, Logger::Loggable<Logger::Id::filter> {
 public:
   ProtocolClient();
 
@@ -48,10 +58,161 @@ private:
   bool haveCiliumPrivilegedService() const { return isOpen(); }
 
   ssize_t transact(MessageHeader& req, size_t req_len, const void* data, size_t datalen, int* fd,
-                   Response& resp, void* buf = nullptr, size_t bufsize = 0, bool assert = true);
+                   Response& resp, void* buf = nullptr, size_t buf_size = 0);
 
-  pthread_mutex_t call_mutex_;
-  uint32_t seq_;
+  std::atomic<uint32_t> seq_;
+  Thread::MutexBasicLockable call_mutex_;
+
+  class Waiter {
+  public:
+    Waiter() = default;
+
+    ssize_t recvFdMsg(ProtocolClient& client) {
+      return size_ = client.recvFdMsg(&resp_, sizeof(resp_), buf_, sizeof(buf_), &fd_);
+    }
+
+    // Returns non-zero sequence number after a response has been received.
+    uint32_t seq() const { return resp_.hdr_.msg_seq_; }
+
+    // Returns received message type
+    MessageType msgType() const { return static_cast<MessageType>(resp_.hdr_.msg_type_); }
+
+    // validate checks that the received sequence number matches the expected one
+    // Returns zero if no respnse has yet been received.
+    ssize_t validate(uint32_t expected_seq_n) const {
+      auto received_seq = seq();
+      if (received_seq) {
+        RELEASE_ASSERT(received_seq == expected_seq_n,
+                       fmt::format("waiter: invalid response sequence: {} != {}", received_seq,
+                                   expected_seq_n));
+        return size_;
+      }
+      return 0;
+    }
+
+    ssize_t getResponse(Response& resp, void* buf, size_t buf_size, int* fd) const {
+      ssize_t size = size_;
+      if (size_t(size) > sizeof(resp)) {
+        auto copy_size = size_t(size) - sizeof(resp);
+        if (copy_size > buf_size) {
+          // truncate response
+          size -= copy_size - buf_size;
+          copy_size = buf_size;
+        }
+        memcpy(buf, buf_, copy_size); // NOLINT(safe-memcpy)
+      }
+      resp = resp_;
+      if (fd) {
+        *fd = fd_;
+      }
+
+      return size;
+    }
+
+    Waiter& operator=(Waiter& other) {
+      size_ = other.size_;
+      fd_ = other.fd_;
+      resp_ = other.resp_;
+      if (size_ > ssize_t(sizeof(resp_))) {
+        size_t copy_size = size_t(size_) - sizeof(resp_);
+        if (copy_size <= sizeof(buf_)) {
+          memcpy(buf_, other.buf_, copy_size); // NOLINT(safe-memcpy)
+        }
+      }
+      return *this;
+    }
+
+    void clear() {
+      size_ = 0;
+      fd_ = -1;
+      resp_ = {};
+    }
+
+  private:
+    ssize_t size_{}; // non-zero after a the response has been received
+    int fd_;
+    Response resp_;
+    char buf_[RESPONSE_BUF_SIZE];
+  };
+
+  // waiterReceive is declared as noexcept to guarantee it will return normally, rather than via an
+  // exception, if the program continues running. This allows for safe removal of the Waiter from
+  // Waiters before the Waiter is destructed.
+  ssize_t waiterReceive(Waiter&, uint32_t seq) noexcept;
+
+  using WaitersMap = absl::flat_hash_map<uint32_t, Waiter*>;
+
+  class Waiters {
+  public:
+    void insert(uint32_t seq, Waiter* waiter) {
+      Thread::LockGuard guard(mutex_);
+      auto ret = map_.emplace(seq, waiter);
+      RELEASE_ASSERT(ret.second, "waiter emplace failed");
+    }
+
+    void remove(uint32_t seq) {
+      Thread::LockGuard guard(mutex_);
+      map_.erase(seq);
+    }
+
+    // wait waits for either a response to be received, or be woken up to become a receiver.
+    // Returns non-zero if there is a response, zero if the receiver quit.
+    ssize_t wait(const Waiter& waiter, uint32_t seq) {
+      Thread::LockGuard guard(mutex_);
+      while (waiter.seq() == 0 && is_receiver_active_) {
+        cond_.wait(mutex_);
+      }
+      return waiter.validate(seq);
+    }
+
+    // wakeUp passes the reveived data to the waiter with sequence number 'seq' and wakes all
+    // waiters.
+    void wakeUp(Waiter& receiver) {
+      {
+        Thread::LockGuard guard(mutex_);
+        auto seq = receiver.seq();
+        auto it = map_.find(receiver.seq());
+        RELEASE_ASSERT(it != map_.end(), fmt::format("no waiter found for seq {}", seq));
+        // copy received data to the found waiter
+        *it->second = receiver;
+        // clear the waiter of the current receiver
+        receiver.clear();
+      }
+      cond_.notifyAll();
+    }
+
+    // Called when a thread successfully acquires call_mutex_
+    // Returns zero if the called should enter the receive loop.
+    ssize_t setReceiverActiveIfNoResponse(const Waiter& waiter, uint32_t seq) {
+      Thread::LockGuard guard(mutex_);
+      if (waiter.seq()) {
+        auto size = waiter.validate(seq);
+        if (size != 0) {
+          return size;
+        }
+      }
+      is_receiver_active_ = true;
+      return 0;
+    }
+
+    // This is called by the thread that just released the call_mutex_.
+    // Wakes up waiters to become the next receiver.
+    void signalReceiverOpen() {
+      {
+        Thread::LockGuard guard(mutex_);
+        is_receiver_active_ = false;
+      }
+      cond_.notifyAll();
+    }
+
+  private:
+    Thread::MutexBasicLockable mutex_;
+    Thread::CondVar cond_;
+    WaitersMap map_ ABSL_GUARDED_BY(mutex_);
+    bool is_receiver_active_ ABSL_GUARDED_BY(mutex_) = false;
+  };
+
+  Waiters waiters_;
 };
 
 using Singleton = Envoy::ThreadSafeSingleton<ProtocolClient>;

starter/privileged_service_protocol.h
@@ -75,16 +75,13 @@ struct MessageHeader {
   MessageHeader(MessageType t) : msg_type_(t) {}
 };
 
-// Response passes the return value and errno code from the system call, followed by optional
-// data depending on the request type. Note that file descriptor return value is passed using the
-// message control channel (ref. man 2 recvmsg).
+// Response passes the return value and errno code from the system call.
+// Note that file descriptor return value is passed using the message control channel (ref. man 2
+// recvmsg).
 struct Response {
-  Response() : hdr_(TypeResponse) {}
-
-  struct MessageHeader hdr_;
-  int return_value_;
-  int errno_;
-  uint8_t data_[];
+  struct MessageHeader hdr_{};
+  int return_value_ = 0;
+  int errno_ = 0;
 };
 
 // Dump requests consists only of the message header, but with the TYPE_DUMP_REQUEST.