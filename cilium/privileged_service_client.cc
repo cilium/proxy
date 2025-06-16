90c5ed94f3a41c0fd0aaf58c46eac484cfb450a6
Jarno Rajahalme <jarno@isovalent.com>
2025-06-16 13:01:35 +0200

    cilium: Reduce locking in privileged service client
    Change the calls to "privileged services" to not take a mutex for sending
    requests.
    
    Set up a `Waiter` for each caller so that the active receiver can pass
    responses to them. Concurrent waiters coordinate under a mutex selecting
    a designated receiver, who then receives responses (without holding
    locks) until it receives its own response. Other responses are passed to
    other waiters.
    
    Any changes to this code should be tested with a performance tool
    maximally loading all the available worker threads (e.g., `wrk`).
    
    This partially addresses the performance regression introduced in #315.
    
    Fixes: #315
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- cilium/privileged_service_client.cc
+++ cilium/privileged_service_client.cc
@@ -7,7 +7,6 @@
 #include <asm-generic/socket.h>
 #include <linux/capability.h>
 #include <linux/limits.h>
-#include <pthread.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -22,6 +21,7 @@
 #include "envoy/api/os_sys_calls_common.h"
 
 #include "source/common/common/assert.h"
+#include "source/common/common/lock_guard.h"
 #include "source/common/common/logger.h"
 
 #include "starter/privileged_service_protocol.h"
@@ -30,8 +30,7 @@ namespace Envoy {
 namespace Cilium {
 namespace PrivilegedService {
 
-ProtocolClient::ProtocolClient()
-    : Protocol(CILIUM_PRIVILEGED_SERVICE_FD), call_mutex_(PTHREAD_MUTEX_INITIALIZER), seq_(0) {
+ProtocolClient::ProtocolClient() : Protocol(CILIUM_PRIVILEGED_SERVICE_FD), seq_(0) {
   // Check that the Envoy process isn't running with privileges.
   // The only exception is CAP_NET_BIND_SERVICE (if explicitly excluded from being dropped).
   RELEASE_ASSERT((getCapabilities(CAP_EFFECTIVE) & ~(1UL << CAP_NET_BIND_SERVICE)) == 0 &&
@@ -39,6 +38,7 @@ ProtocolClient::ProtocolClient()
                  "cilium-envoy running with privileges, exiting");
 
   if (!checkPrivilegedService()) {
+    ENVOY_LOG(warn, "Cilium privileged service not present");
     // No Cilium privileged service detected
     close();
   }
@@ -57,47 +57,133 @@ ProtocolClient::ProtocolClient()
 
 ssize_t ProtocolClient::transact(MessageHeader& req, size_t req_len, const void* data,
                                  size_t data_len, int* fd, Response& resp, void* buf,
-                                 size_t bufsize, bool assert) {
-  uint32_t expected_response_type = resp.hdr_.msg_type_;
+                                 size_t buf_size) {
+  RELEASE_ASSERT(buf_size <= RESPONSE_BUF_SIZE, "ProtocolClient::transact: invalid bufsize");
+  uint32_t seq;
 
-  // Serialize calls to cilium privileged service
-  int rc = pthread_mutex_lock(&call_mutex_);
-  RELEASE_ASSERT(rc == 0, "pthread_mutex_lock");
+  // get next atomic sequence number
+  do {
+    seq = ++seq_;
+  } while (seq == 0); // zero is reserved for "no response"
+  req.msg_seq_ = seq;
 
-  req.msg_seq_ = ++seq_;
+  // Set up a waiter in the stack before we send anything so that the waiter exists as soon as it is
+  // possible for a concurrent receiver to receive the response.
+  Waiter waiter;
+  insert(seq, &waiter);
+
+  // send message after a waiter has been established.
   ssize_t size = sendFdMsg(&req, req_len, data, data_len, *fd);
-  if (!assert && size_t(size) != req_len + data_len) {
-    goto out;
-  }
-  RELEASE_ASSERT(size != 0, "Cilium privileged service closed pipe");
-  RELEASE_ASSERT(size > 0, "Cilium privileged service send failed");
 
-  size = recvFdMsg(&resp, sizeof(resp), buf, bufsize, fd);
-  if (!assert && size_t(size) < sizeof(resp)) {
-    goto out;
-  }
-  RELEASE_ASSERT(size != 0, "Cilium privileged service closed pipe");
-  RELEASE_ASSERT(size < 0 || size_t(size) >= sizeof(resp),
-                 "Cilium privileged service truncated response");
-  RELEASE_ASSERT(resp.hdr_.msg_seq_ == req.msg_seq_,
-                 "Cilium privileged service response out of sequence");
-  RELEASE_ASSERT(resp.hdr_.msg_type_ == expected_response_type,
-                 "Cilium privileged service unexpected response type");
+  if (size > 0) {
+    RELEASE_ASSERT(size_t(size) == req_len + data_len, "truncated request");
 
-out:
-  rc = pthread_mutex_unlock(&call_mutex_);
-  RELEASE_ASSERT(rc == 0, "pthread_mutex_unlock");
-  return size;
+    // receive removes the waiter from 'waiters_' before returning
+    receive(waiter, seq);
+  } else {
+    remove(seq);
+  }
+
+  return waiter.getResponse(seq, resp, buf, buf_size, fd);
+}
+
+// receive
+// 1. Waits to become the receiver, checking for a response one each wake-up
+// 2. Loops receiving responses when becoming the exclusive receiver,
+//    passing resonses to other waiters until its own response is received.
+// 3. Removes the waiter from 'waiters_' before returning.
+void ProtocolClient::receive(Waiter& waiter, uint32_t seq) noexcept {
+  // Loop waiting until we have a response or become the receiver.
+  // 'mutex_' is released when exiting the loop.
+  bool done = false;
+  bool receiver_active;
+  {
+    Thread::LockGuard guard(mutex_);
+    while (true) {
+      // Check if we have our response.
+      if (waiter.seq() != 0) {
+        waiters_.erase(seq);
+        receiver_active = is_receiver_active_;
+        done = true;
+        break;
+      }
+
+      // Check if we can become the receiver.
+      if (!is_receiver_active_) {
+        receiver_active = is_receiver_active_ = true;
+        break;
+      }
+
+      // 'mutex_' is released for the duration of the wait.
+      wait();
+    }
+  }
+
+  // mutex_ not held any more
+  // Return if done
+  if (done) {
+    if (!receiver_active) {
+      // Notify another waiter (if any) to possibly become the new receiver.
+      // This sure there always is a receiver if there are any waiters.
+      notifyOne();
+    }
+    return;
+  }
+
+  // No locks are held, but we just exclusively set the is_receiver_active_ = true above.
+  // Receiver accesses it's own waiter (the 'waiter') without locking.
+  // Other waiters are accessed only while holding 'mutex_'.
+
+  // Receive until we have a response or an error
+  while (true) {
+    ssize_t size = waiter.recvFdMsg(*this);
+    if (size < 0) {
+      ENVOY_LOG(debug, "privileged service failed with {} (errno {})", size, errno);
+      break;
+    }
+
+    // Is the response for us?
+    if (waiter.seq() == seq) {
+      break;
+    }
+
+    // The response is for one of the waiters, pass it on
+    {
+      Thread::LockGuard guard(mutex_);
+      auto it = waiters_.find(waiter.seq());
+      RELEASE_ASSERT(it != waiters_.end(), fmt::format("no waiter found for seq {}", waiter.seq()));
+      // copy received data to the found waiter
+      *it->second = waiter;
+      // clear the waiter of the current receiver
+      waiter.clear();
+    }
+
+    // have to notify all waiters for the right one to be woken up from the wait.
+    notifyAll();
+  }
+
+  // Pass receiver duties to one of the other waiters & remove the waiter from 'waiters_' while we
+  // still have the mutex.
+  {
+    Thread::LockGuard guard(mutex_);
+    is_receiver_active_ = false;
+    waiters_.erase(seq);
+  }
+
+  // wake up one waiter to take the receiver role
+  notifyOne();
 }
 
 bool ProtocolClient::checkPrivilegedService() {
   // Dump the effective capabilities of the privileged service process
   DumpRequest req;
   Response resp;
-  uint8_t buf[1024];
+  uint8_t buf[RESPONSE_BUF_SIZE];
   int fd = -1;
-  ssize_t size = transact(req.hdr_, sizeof(req), nullptr, 0, &fd, resp, buf, sizeof(buf), false);
+
+  ssize_t size = transact(req.hdr_, sizeof(req), nullptr, 0, &fd, resp, buf, sizeof(buf));
   if (size < ssize_t(sizeof(resp))) {
+    ENVOY_LOG_MISC(warn, "Cilium privileged service detection failed with return code: {}", size);
     return false;
   }
   std::string str(reinterpret_cast<char*>(buf), size - sizeof(resp));
